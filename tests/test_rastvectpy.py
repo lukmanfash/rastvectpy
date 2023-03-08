72a997746e458f5a3e5912dcd2131789cbb31783
Lukman Fashina <lukmanfashina@yahoo.com>
2023-03-08 08:57:20 -0500

    initial commit

---
+++ tests/test_rastvectpy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `rastvectpy` package."""
+
+
+import unittest
+
+from rastvectpy import rastvectpy
+
+
+class TestRastvectpy(unittest.TestCase):
+    """Tests for `rastvectpy` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
