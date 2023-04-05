5f051b8f87a82df772fd0eaa702d987bc6c0b81b
Lukman Fashina <lukmanfashina@yahoo.com>
2023-04-05 00:20:45 -0400

    initialcom

--- rastvectpy/rastvectpy.py
+++ rastvectpy/rastvectpy.py
@@ -1,1 +1,9 @@
 """Main module."""
+import random
+import string
+
+def generate_random_string(length):
+    """Generate a random string with the specified length."""
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
