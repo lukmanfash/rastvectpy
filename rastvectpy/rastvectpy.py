5c6ffb553b2acfca3198f5420f694cbf9b4317a7
Lukman Fashina <lukmanfashina@yahoo.com>
2023-04-20 11:39:04 -0400

    ipyleaf upd8s

--- rastvectpy/rastvectpy.py
+++ rastvectpy/rastvectpy.py
@@ -7,7 +7,66 @@ import ipyleaflet
 class Map(ipyleaflet.Map):
 
     def __init__(self, center, zoom, **kwargs):
-        super().__init__(center = center, zoom = zoom)
+
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"] = True
+        # print(kwargs)
+        super().__init__(center = center, zoom = zoom, **kwargs)
+
+    def add_search_control(self, position="topleft", **kwargs):
+        """Add a search control to the map.
+
+        Args:
+            kwargs: Keyword arguments to pass to the ipyleaflet.SearchControl constructor.
+        """  
+        if "url" not in kwargs:
+            kwargs["url"] = "https://nominatim.openstreetmap.org/search?format=json&q={s}"      
+        search_control = ipyleaflet.SearchControl(position=position, **kwargs)
+        self.add_control(ipyleaflet.SearchControl(**kwargs))
+
+    def add_draw_control(self, **kwargs):
+        """Add a draw control to the map.
+
+        Args:
+            kwargs: Keyword arguments to pass to the ipyleaflet.DrawControl constructor.
+        """  
+        draw_control = ipyleaflet.DrawControl(**kwargs)
+      
+        draw_control.polyline =  {
+            "shapeOptions": {
+                "color": "#6bc2e5",
+                "weight": 8,
+                "opacity": 1.0
+            }
+        }
+        draw_control.polygon = {
+            "shapeOptions": {
+                "fillColor": "#6be5c3",
+                "color": "#6be5c3",
+                "fillOpacity": 1.0
+            },
+            "drawError": {
+                "color": "#dd253b",
+                "message": "Oups!"
+            },
+            "allowIntersection": False
+        }
+        draw_control.circle = {
+            "shapeOptions": {
+                "fillColor": "#efed69",
+                "color": "#efed69",
+                "fillOpacity": 1.0
+            }
+        }
+        draw_control.rectangle = {
+            "shapeOptions": {
+                "fillColor": "#fca45d",
+                "color": "#fca45d",
+                "fillOpacity": 1.0
+            }
+        }
+        
+        self.add_control(draw_control)
 
 
 def visualize_raster(raster_data):    
