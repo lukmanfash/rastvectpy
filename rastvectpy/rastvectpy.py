80c3ec6f27300f2e0bf48642fb1fe304b77abca7
Lukman Fashina <lukmanfashina@yahoo.com>
2023-05-10 03:44:40 -0400

    multiple updates

--- rastvectpy/rastvectpy.py
+++ rastvectpy/rastvectpy.py
@@ -339,57 +339,63 @@ class Map(ipyleaflet.Map):
         self.add_widget(html, position=position, **kwargs)
 
 
-    # def basemap_demo(self):
-    #     """A demo for using leafmap basemaps."""
-    #     dropdown = widgets.Dropdown(
-    #         options=list(basemaps.keys()),
-    #         value="HYBRID",
-    #         description="Basemaps",
-    #     )
+    def add_basemap_by_dropdown(self):
+        """Add a basemap to the map with a dropdown widget to select the basemap.
+        """
+        
+        from box import Box
+        import xyz_to_leaflet
+        basemaps = Box(xyz_to_leaflet(), frozen_box=True)
+        
+        dropdown = widgets.Dropdown(
+            options=list(basemaps.keys()),
+            value="none",
+            description="Basemaps",
+        )
 
 
-    # def visualize_raster(raster_data):    
-    #     """
-    #     Visualize a raster data using matplotlib.
+    def visualize_raster(raster_data):    
+        """
+        Visualize a raster data using matplotlib.
 
-    #     Parameters:
-    #     raster_data (numpy.ndarray): A 2D array of raster data.
+        Parameters:
+        raster_data (numpy.ndarray): A 2D array of raster data.
 
-    #     Returns:
-    #     None
-    #     """
-    #     # Create a figure and axis object
-    #     fig, ax = plt.subplots()
-    #     # Set the aspect ratio
-    #     ax.set_aspect('equal')
-    #     # Show the raster data as an image
-    #     ax.imshow(raster_data, cmap='gray')
-    #     # Set the x and y axis labels
-    #     ax.set_xlabel('X')
-    #     ax.set_ylabel('Y')
-    #     # Show the plot
-    #     plt.show()
+        Returns:
+        None
+        """
+        # Create a figure and axis object
+        fig, ax = plt.subplots()
+        # Set the aspect ratio
+        ax.set_aspect('equal')
+        # Show the raster data as an image
+        ax.imshow(raster_data, cmap='gray')
+        # Set the x and y axis labels
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        # Show the plot
+        plt.show()
 
 
-    # def visualize_vector(vector_data):
-    #     """
-    #     Visualize a vector data using matplotlib.
+    def visualize_vector(vector_data):
+        """
+        Visualize a vector data using matplotlib.
 
-    #     Parameters:
-    #     vector_data (tuple): A tuple of two 1D arrays representing the x and y coordinates of the vector data.
+        Parameters:
+        vector_data (tuple): A tuple of two 1D arrays representing the x and y coordinates of the vector data.
 
-    #     Returns:
-    #     None
-    #     """
-    #     # Create a figure and axis object
-    #     fig, ax = plt.subplots()
-    #     # Plot the vector data
-    #     ax.quiver(vector_data[0], vector_data[1], color='blue', scale=1, units='xy', width=0.005, headwidth=5, headlength=7)
-    #     # Set the x and y axis limits
-    #     ax.set_xlim([min(vector_data[0])-1, max(vector_data[0])+1])
-    #     ax.set_ylim([min(vector_data[1])-1, max(vector_data[1])+1])
-    #     # Set the x and y axis labels
-    #     ax.set_xlabel('X')
-    #     ax.set_ylabel('Y')
-    #     # Show plot
-    #     plt.show()
+        Returns:
+        None
+        """
+        # Create a figure and axis object
+        fig, ax = plt.subplots()
+        # Plot the vector data
+        ax.quiver(vector_data[0], vector_data[1], color='blue', scale=1, units='xy', width=0.005, headwidth=5, headlength=7)
+        # Set the x and y axis limits
+        ax.set_xlim([min(vector_data[0])-1, max(vector_data[0])+1])
+        ax.set_ylim([min(vector_data[1])-1, max(vector_data[1])+1])
+        # Set the x and y axis labels
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        # Show plot
+        plt.show()
